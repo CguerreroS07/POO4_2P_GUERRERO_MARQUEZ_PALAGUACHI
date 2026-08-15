f0609f8f6be5df929d682f9053337bb0dbfac059
CguerreroS07 <mauricioguerrero2007@gmail.com>
2026-08-15 17:16:38 -0500

    codigo de las activity admin participante

--- app/src/main/java/com/example/poo4_2p_guerrero_marquez_palaguachi/MenuAdministradorActivity.java
+++ app/src/main/java/com/example/poo4_2p_guerrero_marquez_palaguachi/MenuAdministradorActivity.java
@@ -1,6 +1,10 @@
 package com.example.poo4_2p_guerrero_marquez_palaguachi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,48 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MenuAdministradorActivity extends AppCompatActivity {
 
+    private TextView txtBienvenidaAdmin;
+    private Button btnAdministrarPartidos, btnActualizarPuntajes, btnSalirAdmin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_administrador);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtBienvenidaAdmin = findViewById(R.id.txtBienvenidaAdmin);
+        btnAdministrarPartidos = findViewById(R.id.btnAdministrarPartidos);
+        btnActualizarPuntajes = findViewById(R.id.btnActualizarPuntajes);
+        btnSalirAdmin = findViewById(R.id.btnSalirAdmin);
+
+        String nombreAdmin = getIntent().getStringExtra("NOMBRE_USUARIO");
+        if (nombreAdmin != null) {
+            txtBienvenidaAdmin.setText("Bienvenido(a)\n" + nombreAdmin);
+        }
+
+        btnSalirAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
+        btnAdministrarPartidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        btnActualizarPuntajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
         });
     }
 }
--- app/src/main/java/com/example/poo4_2p_guerrero_marquez_palaguachi/MenuParticipanteActivity.java
+++ app/src/main/java/com/example/poo4_2p_guerrero_marquez_palaguachi/MenuParticipanteActivity.java
@@ -1,6 +1,10 @@
 package com.example.poo4_2p_guerrero_marquez_palaguachi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,56 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MenuParticipanteActivity extends AppCompatActivity {
 
+    private TextView txtBienvenida;
+    private Button btnTabla, btnPronosticos, btnMisPronosticos, btnSalir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_participante);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtBienvenida = findViewById(R.id.txtBienvenida);
+        btnTabla = findViewById(R.id.btnTabla);
+        btnPronosticos = findViewById(R.id.btnPronosticos);
+        btnMisPronosticos = findViewById(R.id.btnMisPronosticos);
+        btnSalir = findViewById(R.id.btnSalir);
+
+        String nombreUsuario = getIntent().getStringExtra("NOMBRE_USUARIO");
+        if(nombreUsuario != null) {
+            txtBienvenida.setText("Bienvenido(a)\n" + nombreUsuario);
+        }
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
+        btnTabla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnPronosticos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        btnMisPronosticos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
         });
     }
 }
