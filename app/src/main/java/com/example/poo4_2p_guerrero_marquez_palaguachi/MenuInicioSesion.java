7322feb4ae700068698ef71c45d32c33008fcb92
CguerreroS07 <mauricioguerrero2007@gmail.com>
2026-08-16 16:40:08 -0500

    Reapply "Implementacion de MenuAdministradorActivity en MenuInicioSesion"
    This reverts commit 4be292a449f8782ed84e2b29964239b13da7cadf.

--- app/src/main/java/com/example/poo4_2p_guerrero_marquez_palaguachi/MenuInicioSesion.java
+++ app/src/main/java/com/example/poo4_2p_guerrero_marquez_palaguachi/MenuInicioSesion.java
@@ -1,5 +1,6 @@
 package com.example.poo4_2p_guerrero_marquez_palaguachi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,7 +10,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class MenuInicioSesion extends AppCompatActivity {
-
     private EditText edtUsuario, edtContrasena;
     private Button btnIniciarSesion;
 
@@ -24,23 +24,29 @@ public class MenuInicioSesion extends AppCompatActivity {
 
         btnIniciarSesion.setOnClickListener(v -> {
             try {
-                validarCredenciales(edtUsuario.getText().toString(), edtContrasena.getText().toString());
+                String[] datosUsuario = validarYObtenerDatos(edtUsuario.getText().toString(), edtContrasena.getText().toString());
                 Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MenuInicioSesion.this, MenuAdministradorActivity.class);
+                intent.putExtra("NOMBRE_COMPLETO", datosUsuario[3]);
+                intent.putExtra("TIPO_USUARIO", datosUsuario[4]);
+                startActivity(intent);
+                finish();
+
             } catch (CredencialesInvalidasException e) {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-    private void validarCredenciales(String user, String pass) throws CredencialesInvalidasException {
-        boolean encontrado = false;
+    private String[] validarYObtenerDatos(String user, String pass) throws CredencialesInvalidasException {
+        String[] usuarioEncontrado = null;
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("usuarios.txt")));
             String linea;
             while ((linea = reader.readLine()) != null) {
                 String[] datos = linea.split(",");
                 if (datos[1].equals(user) && datos[2].equals(pass)) {
-                    encontrado = true;
+                    usuarioEncontrado = datos;
                     break;
                 }
             }
@@ -48,9 +54,9 @@ public class MenuInicioSesion extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        if (!encontrado) {
+        if (usuarioEncontrado == null) {
             throw new CredencialesInvalidasException("Usuario o contraseña incorrectos.");
         }
+        return usuarioEncontrado;
     }
 }
